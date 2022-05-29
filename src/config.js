80edc0c065a878b1c1657bed7082126e8fa73dc1
genaro21 <gallardo2147@gmail.com>
2022-05-29 21:59:57 +0200

    routes ¬ controllers

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  database: {
+    url: 'mongodb://localhost/social-new',
+  },
+  json: {
+    secret: 'loquesea',
+  },
+};
+
+module.exports = config;
---
+++ src/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const path = require('path');
+
+const server = express();
+
+//Settings.
+server.set('port', 4600);
+
+//Middlewares.
+server.use(cors());
+server.use(morgan('dev'));
+
+//Routes.
+server.use('/api/user', routes.user);
+server.use('/api/post', routes.post);
+server.use('/api/comment', routes.comment);
+
+//Static folder.
+server.use(express.static(path.join(__dirname, 'statics')));
+
+module.exports = server;
