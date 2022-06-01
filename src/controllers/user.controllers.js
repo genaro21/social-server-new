55020f4a075d19d97e27a45458c15b392ff47f42
genaro21 <gallardo2147@gmail.com>
2022-06-01 12:58:33 +0200

    bcrypt added

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,26 @@
 const models = require('../models');
+const utils = require('../utils');
 
 const signIn = (req, res) => {
   return res.json('SignIn');
 };
-const signUp = (req, res) => {
-  return res.json('SignUp');
+const signUp = async (req, res) => {
+  try {
+    const { file, email, password } = req.body;
+
+    const hash = await utils.bcrypt.encrypt(password);
+
+    const user = {
+      avatar: 'avatar',
+      email,
+      password: hash,
+    };
+
+    const data = await models.user.create(user);
+    return res.status(201).json({ data });
+  } catch (err) {
+    return res.json(err);
+  }
 };
 
 module.exports = {
